0ad2cf3280cdc037f9ffc34e681980a0c3caddde
Matt Rogers <matt@rogers.zone>
2022-12-03 13:58:18 -0500

    Add solution template

---
+++ year_2022/template/problem.py
@@ -0,0 +1,17 @@
+# Advent of Code 2022
+# Day __, Problem _
+# Author: Matt Rogers
+
+
+import sys
+
+
+# Main method
+def main(raw: str) -> str:
+    ...
+
+
+if __name__ == '__main__':
+    puzzle_input = sys.stdin.read()
+    puzzle_output = main(puzzle_input)
+    print(puzzle_output)
