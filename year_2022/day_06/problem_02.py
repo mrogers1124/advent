1dc5019d2750cafb10bff938117d82aaf1162ec6
Matt Rogers <matt@rogers.zone>
2022-12-06 21:28:13 -0500

    Solve day 6

---
+++ year_2022/day_06/problem_02.py
@@ -0,0 +1,24 @@
+# Advent of Code 2022
+# Day 06, Problem 2
+# Author: Matt Rogers
+
+
+import sys
+
+
+# Main method
+def main(raw: str) -> str:
+    buffer = raw.strip()
+    return str(find_marker(buffer, length=14))
+
+
+def find_marker(buffer: str, length) -> int:
+    for n in range(length, len(buffer)):
+        if len(set(buffer[n-length:n])) == length:
+            return n
+
+
+if __name__ == '__main__':
+    puzzle_input = sys.stdin.read()
+    puzzle_output = main(puzzle_input)
+    print(puzzle_output)
